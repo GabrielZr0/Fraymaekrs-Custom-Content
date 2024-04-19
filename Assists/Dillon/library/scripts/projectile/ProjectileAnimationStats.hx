ff55807d546b1687c2a5fd6c06db5023826ba5af
GabrielZr0 <61251586+GabrielZr0@users.noreply.github.com>
2024-04-19 14:37:19 -0300

    Dillon 1.0

--- Assists/Dillon/library/scripts/projectile/ProjectileAnimationStats.hx
+++
@@ -1,4 +0,0 @@
-// Animation stats for Lea's bullet projectile
-{
-	projectile: {endType:AnimationEndType.LOOP, slideOff: true}
-}
--- Assists/Dillon/library/scripts/projectile/ProjectileHitboxStats.hx
+++
@@ -1,6 +0,0 @@
-// Hitbox stats for Assist Template Projectile
-{
-	projectile: {
-		hitbox0: { damage: 10, knockbackGrowth: 10, baseKnockback: 100, hitstop:10, hitstun: -1, angle: 40, flinch:true, element:AttackElement.NORMAL, reversibleAngle: false, directionalInfluence: false, reflectable: true, shieldable: false }
-	}
-}
